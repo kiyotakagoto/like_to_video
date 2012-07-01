275b90766d47fcaad10d10dc05a3688a3dbfbc10
KiyotakaGoto <spain.xavi.h@gmail.com>
2012-07-01 22:58:10 +0900

    write test code

---
+++ js/test.js
@@ -0,0 +1,13 @@
+function poll_video_current_time ( event ) {
+    if ( event === 1 ) {
+        setInterval( function ( event ) {
+            var player = document.getElementById('myytplayer');
+            console.log( player.getCurrentTime() );
+        }, 300);
+    }
+};
+
+function onYouTubePlayerReady ( player_api_id ) {
+    var player = document.getElementById('myytplayer');
+    player.addEventListener('onStateChange', 'poll_video_current_time');
+};
